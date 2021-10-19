32e4db405a99d926b155f114ddcf3c2c6bd764a5
Gaurav <gauravsharma90.de@gmail.com>
2021-10-19 23:33:58 +0200

    initial commit

---
+++ Demo.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Demo {
+	//test
+	public static void main(String[] args) {
+		try {
+		//set property contains two argumant 'key','property'
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Gaurav Sharma\\eclipse-workspace\\Introduction\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com/");
+		driver.findElementByXPath("//button[text()='Allow All Cookies']").click();
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+		driver.findElementByXPath("//input[@id='email']").sendKeys("gaurav.link90@gmail.com");
+		driver.findElementByXPath("//input[@id='pass']").sendKeys("gaurav");
+		
+		System.out.println(driver.getCurrentUrl());//Validate If you are landed to correct url
+		System.out.println(driver.getPageSource());//Print the page source
+		driver.quit();
+		}catch(Exception e) 
+		{
+		System.out.println(e.getMessage());
+			
+		}
+		
+
+	}
+}
+
+
